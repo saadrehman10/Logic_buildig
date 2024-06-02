091abaab185d6f8666d58896dc6b7c8b0717d68a
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-02 14:08:08 +0500

    changes

---
+++ dart.dart
@@ -0,0 +1,40 @@
+String ReverseString(String value) {
+  List<String> temp = [];
+  List<String> output = [];
+  String tempStr = '';
+
+  for (int i = 0; i < value.length; i++) {
+    temp.add(value[i]);
+  }
+  for (int i = temp.length - 1; i >= 0; i--) {
+    output.add(temp[i]);
+  }
+  for (int i = 0; i < output.length; i++) {
+    print(output[i]);
+    tempStr += output[i];
+  }
+  return tempStr;
+}
+
+void main() {
+  // int x = 11211;
+  // print(
+  //     x.toString().split('').reversed.join('') == x.toString() ? true : false);
+  // List<String> array = x.toString().split('');
+  // var temp = [1];
+  // var a = temp[0];
+  // switch(a) {
+  //   case 0:
+  //     print('hello')
+  //     break;
+  //   default:
+  //     print('hellod');
+  // }
+  // print(ReverseString('value'));
+  var s = 'hello'.split('');
+  // for (int i = s.length - 1; i >= 0; i--) {
+  //   String output = s.removeAt(i);
+  //   s.insert(0, output);
+  // }
+  print(s.reversed);
+}
