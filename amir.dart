bddbcd6822be9119e6130086fbb71e4b463ef90d
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-10 22:11:56 +0500

    update

--- amir.dart
+++ amir.dart
@@ -1,5 +1,20 @@
-void main(){
-  List fruit=[];
-  fruit.addAll(['apple','banana']);
-  print(fruit);
+// void main(){
+//   List fruit=[];
+//   fruit.addAll(['apple','banana']);
+//   print(fruit);
+// }
+
+void main() {
+//   Question:
+// Given a map representing a product with keys "name", "price", and
+// "quantity", write Dart code to check if the product is in stock. If the quantity is
+// greater than 0, print "In stock", otherwise print "Out of stock"
+
+  Map product = {"name": "amir", "price": "6000", "quantity": 4};
+  print(product["name"]);
+  if (product["quantity"] >= 0) {
+    print("inStock");
+  } else {
+    print("Out of stock");
+  }
 }
--- d
+++
@@ -1,5 +0,0 @@
-void main (){
-    List fruit=[];
-  fruit.add('apple');
-  print(fruit);
-}
