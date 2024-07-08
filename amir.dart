9eddd56f2c79013fc43c8bf704afb494bce5b2a4
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-07-08 21:53:14 +0500

    update

--- amir.dart
+++ amir.dart
@@ -10,11 +10,16 @@ void main() {
 // "quantity", write Dart code to check if the product is in stock. If the quantity is
 // greater than 0, print "In stock", otherwise print "Out of stock"
 
-  Map product = {"name": "amir", "price": "6000", "quantity": 0};
-  print(product["name"]);
-  if (product["quantity"] > 0) {
-    print("inStock");
-  } else {
-    print("Out of stock");
-  }
+  // Map product = {"name": "amir", "price": "6000", "quantity": 0};
+  // print(product["name"]);
+  // if (product["quantity"] > 0) {
+  //   print("inStock");
+  // } else {
+  //   print("Out of stock");
+  // }
+  int? a = 1;
+  int? b = 2;
+  print(a ?? 0);
+  print(b ?? 0);
+  print(a ?? b);
 }
